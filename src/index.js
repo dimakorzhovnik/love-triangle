448698c1611d3881bf03f292016551fa911575f7
dimakorzhovnik <korzhovnik.dima@gmail.com>
2018-02-20 12:11:51 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let count = 0;
+  for (let i = 0; i  <= preferences.length; i++) {
+    if( preferences[preferences[preferences[i-1]-1]-1] === i){
+      count++;
+          }
+      }   
+  
+      //return count/3|0;
+      return (count - count%3)/3;   
+    };
+
